6443873d431193711f8bd9dc5a9adf011a8ab7e6
Lorena Jinchuk <lorena@iugo.com.uy>
2015-05-20 15:50:48 -0300

    landing page

---
+++ app/views/index.blade.php
@@ -0,0 +1,61 @@
+<html lang="es-UY">
+
+	<head>
+	  <title>Link</title>
+	  <link href="{{ URL::to('/css/style.css') }}" media="all" rel="stylesheet" type="text/css" />
+	  <link href="{{ URL::to('/css/responsive.css') }}" media="all" rel="stylesheet" type="text/css" />
+
+	</head>
+
+	<body>
+
+		<div class = "main">
+			<div class="section1">
+				<div class="innerhead">
+					<div class = 'logo'></div>
+					<a href="#contactenos" class="contactenos"></a>
+				</div>
+				<div class="section1-content">
+					<h1>DE SU TIEMPO, NOS ENCARGAMOS NOSOTROS</h1>
+					<p>Integrando sistemas de gestión con plataformas de comercio electrónico, 
+					potenciando las ventas de su companía y permitiendo aprovechar mejor sus recuros.</p>
+					<a class="flecha" href="#nosotros"></a>
+				</div>
+			</div>
+			<div class="section2" id="nosotros">
+				<div class="section2-content">
+					<h2>Sobre Nosotros</h2>
+					<p>Somos un grupo de estudiantes avanzados de la carrera Ingeniería de Sistemas de la Universidad ORT del Uruguay.  Nos apasiona buscar soluciones tecnológicas a problemas cotidianos. Desarrollamos productos innovadores que mejoran la experiencia de gestionar su empresa.</p>
+					<ul class="author_list">
+				      <li> <a href="#"> <img src="/img/nosotros1.jpg" alt=""> <br>
+				        LORENA JINCHUK <br>
+					  </a> </li>
+				      <li> <a href="#"> <img src="/img/nosotros1.jpg" alt=""> <br>
+				        ALEJANDRO TOCAR <br>
+					  </a> </li>
+				      <li> <a href="#"> <img src="/img/nosotros1.jpg" alt=""> <br>
+				        SIMON SEITUNE MACADAR <br>
+					  </a> </li>
+				      <li> <a href="#"> <img src="/img/nosotros1.jpg" alt=""> <br>
+				        GAL BRENNER <br>
+					  </a> </li>
+				      <li> <a href="#"> <img src="/img/nosotros1.jpg" alt=""> <br>
+				        ALAN BUKSTEIN <br>
+					  </a> </li>				      
+				    </ul>
+				</div>
+			</div>
+			<div class="section3" id = "contactenos">
+				<div class="section3-content">
+					<div class="tittle">Contácto<div class="linea"></div></div>
+					
+					<div class="email">lorejinchuk@gmail.com</div>
+				</div>
+				
+			</div>
+		</div>
+		<footer>
+			<div class="logo-footer"></div>
+		</footer>
+	</body>
+</html>
